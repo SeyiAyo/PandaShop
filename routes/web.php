f96e1350687fcbcd613dabd38feb07c2fdf34513
SeyiAyo <110205697+SeyiAyo@users.noreply.github.com>
2024-08-26 23:21:13 +0100

    load templates and configure Landing page

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Default Laravel landing page
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
